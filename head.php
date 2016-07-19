b6ff732f7c94dff5afcee9962f6e061d8b591217
Jessica B. Caballero <caballerojessica0604@gmail.com>
2016-07-19 16:53:17 -0400

    july 19

--- head.php
+++ head.php
@@ -12,4 +12,54 @@
 	<link href="css/colorbox/colorbox.css" rel="stylesheet">
 	<link href="css/morris.css" rel="stylesheet"/>	
 	<link href="css/endless-skin.css" rel="stylesheet">
+	<link href="css/chosen/chosen.min.css" rel="stylesheet"/>
+	<link href="css/datepicker.css" rel="stylesheet"/>
+	<link href="css/bootstrap-timepicker.css" rel="stylesheet"/>
+	<link href="css/jquery.tagsinput.css" rel="stylesheet"/>
+	<link href="css/bootstrap-wysihtml5.css" rel="stylesheet"/>
 </head>
+<body class="overflow-hidden">
+	<div id="overlay" class="transparent"></div>
+
+	<a href="#" id="theme-setting-icon"><i class="fa fa-cog fa-lg"></i></a>
+	<div id="theme-setting">
+		<div class="title">
+			<strong class="no-margin">Skin Color</strong>
+		</div>
+		<div class="theme-box">
+			<a class="theme-color" style="background:#323447" id="default"></a>
+			<a class="theme-color" style="background:#efefef" id="skin-1"></a>
+			<a class="theme-color" style="background:#a93922" id="skin-2"></a>
+			<a class="theme-color" style="background:#3e6b96" id="skin-3"></a>
+			<a class="theme-color" style="background:#635247" id="skin-4"></a>
+			<a class="theme-color" style="background:#3a3a3a" id="skin-5"></a>
+			<a class="theme-color" style="background:#495B6C" id="skin-6"></a>
+		</div>
+		<div class="title">
+			<strong class="no-margin">Sidebar Menu</strong>
+		</div>
+		<div class="theme-box">
+			<label class="label-checkbox">
+				<input type="checkbox" checked id="fixedSidebar">
+				<span class="custom-checkbox"></span>
+				Fixed Sidebar
+			</label>
+		</div>
+	</div><!-- /theme-setting -->
+	
+	<div id="wrapper" class="preload">
+		<div id="top-nav" class="skin-6 fixed">
+			<div class="brand">
+				<span>EMB VII</span>
+				<span class="text-toggle">Doc. Routing<span>
+			</div><!-- /brand -->
+			<button type="button" class="navbar-toggle pull-left" id="sidebarToggle">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<button type="button" class="navbar-toggle pull-left hide-menu" id="menuToggle">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
